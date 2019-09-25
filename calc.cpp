5fc33ee1fe22c40ee55413466ee33fe1b8c72cf2
yoshiyuki <yoshi.isozaki@fuji.waseda.jp>
2019-09-25 18:21:00 +0900

    create calc.cpp calc.h

---
+++ calc.cpp
@@ -0,0 +1,21 @@
+#include "calc.h"
+
+int Calc::add()
+{
+    return num1 + num2;
+}
+
+int Calc::sub()
+{
+    return num1 - num2;
+}
+
+int Calc::mul()
+{
+    return num1 * num2;
+}
+
+int Calc::div()
+{
+    return num1 / num2;
+}
---
+++ calc.h
@@ -0,0 +1,15 @@
+#ifndef _CALC_H_
+#define _CALC_H_
+class Calc{
+private:
+    int add();
+    int sub();
+    int mul();
+    int div();
+
+public:
+    int mul1;
+    int mul2;
+};
+
+#endif // _CALC_H_
